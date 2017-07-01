2ad22295d089d7120052526c5a77943597568a30
Ash Duckett <ash.duckett@outlook.com>
2017-07-01 12:42:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+
+
+
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -10,13 +14,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true
+    showSearchPage: false
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
+
+          // Get this into a component named SearchBar
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
@@ -24,10 +30,15 @@ class BooksApp extends React.Component {
                 <input type="text" placeholder="Search by title or author"/>
               </div>
             </div>
+            
             <div className="search-books-results">
               <ol className="books-grid"></ol>
             </div>
           </div>
+          // Maybe two components? Extract to second and maybe third file.
+          // I think you can set the state above in the parent. Check vid.
+
+
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -40,6 +51,8 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
+
+
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
@@ -56,12 +69,19 @@ class BooksApp extends React.Component {
                           <div className="book-title">To Kill a Mockingbird</div>
                           <div className="book-authors">Harper Lee</div>
                         </div>
+
+
                       </li>
+
+
                       <li>
+
+
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
                             <div className="book-shelf-changer">
+
                               <select>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
@@ -69,6 +89,7 @@ class BooksApp extends React.Component {
                                 <option value="read">Read</option>
                                 <option value="none">None</option>
                               </select>
+
                             </div>
                           </div>
                           <div className="book-title">Ender's Game</div>
@@ -78,6 +99,9 @@ class BooksApp extends React.Component {
                     </ol>
                   </div>
                 </div>
+
+
+
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+/* Why does App work? Our component is BooksApp */
 import App from './App'
+
+/*import BooksApp from './App'*/
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
