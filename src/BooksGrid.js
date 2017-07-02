383ec0b3d8a5dfaef3803b246f1aa94d4d2cff7b
Ash Duckett <ash.duckett@outlook.com>
2017-07-02 03:29:31 +0100

    Switch to using search api function.

---
+++ src/BooksGrid.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+// This is here so that each book can manage its own state - particularly the
+// shelf it should be on.
+class Book extends React.Component {
+
+    render() {
+        
+        let book = this.props.book
+
+        return(
+        <li key={book.id}>
+            <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                  <div className="book-shelf-changer">
+                    <select value={book.shelf} onChange={(event) => this.props.onUpdateBook(event, book, book.shelf)}>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors[0] ? book.authors[0] : ''}</div> 
+              </div>
+            </li>
+        )
+    }
+}
+
+
+class BooksGrid extends React.Component {
+
+
+
+  render() {
+    // Is having such a small component bad practice?
+    // Have a think
+    let books
+
+    if(!(this.props.books && this.props.books.error)) {
+        books = this.props.books
+    } else {
+        books = []
+    }
+    // You never render a single book on its own. The only reason for having the book as a separate component
+    // is so that you can attach a state to it. Is this required?
+    return(
+      <ol className="books-grid">
+        {
+            books.map((book) => (
+            <Book onUpdateBook={this.props.onUpdateBook} book={book ? book : null} key={book.id}/>
+        ))
+        }
+      </ol>
+    )
+  }
+}
+
+export default BooksGrid
--- src/SearchArea.js
+++ src/SearchArea.js
@@ -1,67 +1,38 @@
 import React from 'react'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import BooksGrid from './BooksGrid'
+import * as BooksAPI from './BooksAPI'
 
-class BooksGrid extends React.Component {
-  render() {
-    return(
-      // Should do something with authors, it being an array and all
-      // THEN: Tidy up
-      // THEN: Get the API working
-      <ol className="books-grid">
-        {this.props.books.map((book) => (
-          <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors[0]}</div> 
-              </div>
-            </li>
-        ))}
-      </ol>
-    )
-  }
-}
+/*
+  On initially rendering the search area, show nothing.
 
+*/
 
 
 class SearchArea extends React.Component {
   // The state for this component is just going to need a continually updated query.
   // It's here because each component should manage its own state.
   state = {
-    query: ''
+    query: '',
+    filteredBooks: null
   }
 
   handleChange = (query) => {
     this.setState({query: query})
 
-    // if the query is empty, return this.props.books, otherwise return the filtered ones.
-    // You'll need to add the regexp stuff to make the magic happen.
+    if(query.trim() !== '') {
+      BooksAPI.search(query, 20).then(books => this.setState({
+        filteredBooks: books
+      }))
+    } else {
+      this.setState({
+        filteredBooks: []
+      })
+    }
   }
 
   render() {
-
-    let showingBooks
-
-    if(this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      showingBooks = this.props.books.filter((book) => match.test(book.title))
-    } else {
-      showingBooks = this.props.books
-    }
-
-
     return(
       // Find out about the function call because you want to set the state on the BooksApp component.
       <div className="search-books">
@@ -72,7 +43,7 @@ class SearchArea extends React.Component {
           </div>
         </div>
         <div className="search-books-results">
-          <BooksGrid books={showingBooks}/>
+          <BooksGrid onUpdateBook={this.props.onUpdateBook} books={this.state.filteredBooks ? this.state.filteredBooks : []}/>
         </div>          
       </div>
     )
@@ -80,15 +51,3 @@ class SearchArea extends React.Component {
 }
 
 export default SearchArea
-
-
-/*
-
-  How do I get the search area working?
-
-  When you type, you want to update the display.
-
-
-
-
-*/
